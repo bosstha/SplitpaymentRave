f9f3bb5b768486ec71d8f1c83076308f417c59c4
Josef <bosetha68@mail.com>
2019-05-26 14:39:12 +0100

    rave

---
+++ rave.js
@@ -0,0 +1,40 @@
+{
+    "PBFPubKey": "FLWPUBK-4e581ebf8372cd691203b27227e2e3b8-X",
+    "cardno": "5438898014560229",
+    "cvv": "890",
+    "expirymonth": "09",
+    "expiryyear": "19",
+    "currency": "NGN",
+    "country": "NG",
+    "amount": "10",
+    "email": "user@gmail.com",
+    "phonenumber": "0902620185",
+    "firstname": "temi",
+    "lastname": "desola",
+    "subaccounts": [
+      {
+        "id": "RS_D87A9EE339AE28BFA2AE86041C6DE70E",
+        "transaction_split_ratio": "2"
+      },
+      {
+        "id": "RS_344DD49DB5D471EF565C897ECD67CD95",
+        "transaction_split_ratio": "3"
+      },
+      {
+        "id": "RS_839AC07C3450A65004A0E11B83E22CA9",
+        "transaction_split_ratio": "5"
+      }
+    ],
+    "IP": "355426087298442",
+    "txRef": "MC-",
+    "meta": [
+      {
+        "metaname": "flightID",
+        "metavalue": "123949494DC"
+      }
+    ],
+    "IP": "355426087298442",
+    "txRef": "MC-" + Date.now(),// your unique merchant reference
+    "redirect_url": "https://rave-webhook.herokuapp.com/receivepayment",
+    "device_fingerprint": "69e6b7f0b72037aa8428b70fbe03986c"
+  }
